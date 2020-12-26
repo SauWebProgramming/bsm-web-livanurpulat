e816bd4c18b0f0b7428efd7bd9454997e7f9dbe7
esraaksu <esra.aksu2@ogr.sakarya.edu.tr>
2020-12-26 12:04:59 +0300

    categories add

---
+++ DenizKabugu/DenizKabugu/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using DenizKabugu.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DenizKabugu.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        public IActionResult Product()
+        {
+            var categories = new List<Category>()
+            {
+                new Category(){CategoryID=1, Name="Çiçek"},
+                new Category(){CategoryID=2, Name="Çikolata"},
+                new Category(){CategoryID=3, Name="Kitap"},
+                new Category(){CategoryID=4, Name="Oyuncak"}
+            };
+            return View(categories);
+        }
+
+        public IActionResult Cicek(Category cicek)
+        {
+            return View(cicek);
+        }
+
+        public IActionResult Cikolata()
+        {
+            return View();
+        }
+
+        public IActionResult Oyuncak()
+        {
+            return View();
+        }
+
+        public IActionResult Kitap()
+        {
+            return View();
+        }
+
+    }
+}
