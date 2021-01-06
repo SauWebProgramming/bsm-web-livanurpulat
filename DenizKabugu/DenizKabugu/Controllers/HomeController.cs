51065a20fe063149ba1e22308370c81b0bed65a6
esraaksu <esra.aksu2@ogr.sakarya.edu.tr>
2021-01-07 01:53:32 +0300

    role manage

--- DenizKabugu/DenizKabugu/Controllers/HomeController.cs
+++ DenizKabugu/DenizKabugu/Controllers/HomeController.cs
@@ -4,12 +4,15 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using DenizKabugu.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace DenizKabugu.Controllers
 {
+    [Authorize]
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
