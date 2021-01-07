e626266af9d83b7c9e9b716e6f3bcb277c85855b
esraaksu <esra.aksu2@ogr.sakarya.edu.tr>
2021-01-07 22:43:49 +0300

    düzenleme

--- DenizKabugu/DenizKabugu/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ DenizKabugu/DenizKabugu/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -30,7 +30,7 @@ namespace DenizKabugu.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
+            _logger.LogInformation("Çıkış yapıldı.");
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
